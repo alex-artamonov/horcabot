e2af35d7be0bdf3fe59b166882f030d90ba4b878
alex-artamonov <alex.artamonov2010@gmail.com>
2024-01-03 23:36:29 +0100

    Get accented letters to work

--- hangman.py
+++ hangman.py
@@ -8,23 +8,46 @@ import pseudographics as pg
 
 FILENAME = "words.txt"
 LINK = "https://www.wordreference.com/es/en/translation.asp?spen="
-ACENTOS = {"E": "eé",
-           "A": "aá",
-           "U": "uüú",
-           "N": "nñ",
-           "I": "ií",
-           "O": "oó"}
+ACENTOS = {"E": "eé", "A": "aá", "U": "uüú", "N": "nñ", "I": "ií", "O": "oó"}
+ABC_DICT = {
+    "B": "B",
+    "C": "C",
+    "D": "D",
+    "F": "F",
+    "G": "G",
+    "H": "H",
+    "J": "J",
+    "K": "K",
+    "L": "L",
+    "M": "M",
+    "P": "P",
+    "Q": "Q",
+    "R": "R",
+    "S": "S",
+    "T": "T",
+    "V": "V",
+    "W": "W",
+    "X": "X",
+    "Y": "Y",
+    "Z": "Z",
+    "E": "EÉ",
+    "A": "AÁ",
+    "U": "UÜÚ",
+    "N": "NÑ",
+    "I": "IÍ",
+    "O": "OÓ",
+}
+
+ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZÁÜÚÑÍÓ"
 
 
 class Hangman:
-    def __init__(self, id: int = 0) -> None:
+    def __init__(self, id: int = 0):
         self.id = id
         self.mistake_nbr = 0
         self.mask = "----"
         self.used = []
-        # self.word = self._get_word()
-        self.word = 'anón'.upper()
-        self.word = 'añan'.upper()
+        self.word = self._get_word()
         self.finished = False
         self.imgs = [
             pg.s0,
@@ -42,21 +65,9 @@ class Hangman:
         ]
         print(self.word)
 
-    def letter_in(self):
-        if self.letter in "AEIOUN":
-            print(ACENTOS[self.letter].upper())
-            for letter in ACENTOS[self.letter].upper():
-                if letter in self.word:
-                    self.letter = letter
-                    return True
-            # return any([acento in self.word for acento in ACENTOS[self.letter].upper()])
-        return self.letter in self.word
-
     def _get_word(self):
-        lst = []
         with open(FILENAME) as read:
-            for line in read:
-                lst.append(line.strip())
+            lst = [line.strip() for line in read.readlines()]
         return choice(lst).upper()
 
     def get_letter(self, prompt: str = "Adivina una letra\n"):
@@ -69,21 +80,25 @@ class Hangman:
         letter = letter.upper()
         if len(letter) > 1:
             raise ValueError("Sólo una letra!")
+        if letter.upper() not in ABC:
+            raise ValueError("Sólo letras!")
         if letter in self.used:
             # output = 'Try again, this letter has been used already.'
             return "Inténtalo de nuevo, esta letra ya ha sido utilizada."
         self.used.append(letter)
         self.letter = letter
-        if self.letter_in():
-            mask_lst = list(self.mask)
-            lst = [i for i in range(len(self.word)) if self.word[i] == self.letter]
-            for c in lst:
-                mask_lst[c] = self.letter
-            self.mask = "".join(mask_lst)
-            if not "-" in mask_lst:
-                self.finished = True
-                output = "Has ganado!\n" + "<b>" + self.word + "</b>"
-                return output
+        if any([c in self.word for c in ABC_DICT[self.letter]]):
+            print(ABC_DICT[self.letter])
+            for letter in ABC_DICT[self.letter]:
+                mask_lst = list(self.mask)
+                lst = [i for i in range(len(self.word)) if self.word[i] == letter]
+                for c in lst:
+                    mask_lst[c] = letter
+                self.mask = "".join(mask_lst)
+                if not "-" in mask_lst:
+                    self.finished = True
+                    output = "Has ganado!\n" + "<b>" + self.word + "</b>"
+                    return output
             return self.mask
         else:
             if len(self.imgs) > self.mistake_nbr + 1:
@@ -115,8 +130,6 @@ class Hangman:
 
 if __name__ == "__main__":
     game = Hangman()
-    # game.finished = True
     game.input = input
     game.display = print
-    # game.start()
     game.play()
